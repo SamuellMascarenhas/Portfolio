8aff6c82f585222123df432dbce78aecfa791ed0
SamuellMascarenhas <samuelmascarenhascontato@gmail.com>
2024-03-19 20:29:32 -0300

    update 19/03/2024

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,10 @@
-/**
-* Template Name: Personal
-* Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
-* Updated: Mar 17 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 
 (function() {
   "use strict";
 
   /**
-   * Easy selector helper function
+   *
+Função auxiliar de seleção fácil
    */
   const select = (el, all = false) => {
     el = el.trim()
